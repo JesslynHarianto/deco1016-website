3227147c36e581c2be54381eeb4dba1ec57b5447
JesslynHarianto <jhar7238@uni.sydney.edu.au>
2019-11-13 21:33:25 +1100

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+const cn = document.getElementById('cn')
+const password = document.getElementById('pass')
+const name = document.getElementById('name')
+
+if(cn.value == "" || password.value=="" || name.value==""){
+alert("No blank values allowed")
+}
+
+if(password.value <= 6){
+    alert("Password must be more than 6")
+}
+
+
+
+
